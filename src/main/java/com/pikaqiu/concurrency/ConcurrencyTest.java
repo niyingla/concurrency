43d0576e3949a38c0c0440af71c3976f093dc4b4
pikaqiu <1047984367@qq.com>
2019-02-23 22:43:51 +0800

    初始化代码

--- src/main/java/com/pikaqiu/concurrency/ConcurrencyTest.java
+++ src/main/java/com/pikaqiu/concurrency/ConcurrencyTest.java
@@ -18,17 +18,27 @@ public class ConcurrencyTest {
     // 同时并发执行的线程数
     public static int threadTotal = 200;
 
+    /**
+     * 计数值
+     */
     public static int count = 0;
 
     public static void main(String[] args) throws Exception {
+        //定义线程池
         ExecutorService executorService = Executors.newCachedThreadPool();
+        //信号量                                   并发数
         final Semaphore semaphore = new Semaphore(threadTotal);
+        //线程计数器
         final CountDownLatch countDownLatch = new CountDownLatch(clientTotal);
+        //循环提交任务
         for (int i = 0; i < clientTotal ; i++) {
             executorService.execute(() -> {
+                //首先引人信号量
                 try {
+                    //启用 （是否允许范围内 超出范围线程等待）
                     semaphore.acquire();
                     add();
+                    //释放
                     semaphore.release();
                 } catch (Exception e) {
                     log.error("exception", e);
@@ -36,7 +46,9 @@ public class ConcurrencyTest {
                 countDownLatch.countDown();
             });
         }
+        //线程计数器等待
         countDownLatch.await();
+        //线程池终止
         executorService.shutdown();
         log.info("count:{}", count);
     }
