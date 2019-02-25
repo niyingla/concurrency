d9ed26165bdeb052d6b729631f8ba916fb74ed59
1047984367@qq.com <as1314520>
2019-02-25 14:46:38 +0800

    修改注释

--- src/main/java/com/pikaqiu/concurrency/example/lock/LockExample1.java
+++ src/main/java/com/pikaqiu/concurrency/example/lock/LockExample1.java
@@ -12,10 +12,14 @@ import java.util.concurrent.Semaphore;
 @ThreadSafe
 public class LockExample1 {
 
-    // 请求总数
+    /**
+     * 请求总数
+     */
     public static int clientTotal = 5000;
 
-    // 同时并发执行的线程数
+    /**
+     * 同时并发执行的线程数
+     */
     public static int threadTotal = 200;
 
     public static int count = 0;
