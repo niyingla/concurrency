d81c52ee632edbce4f1d34154b4d7e63945d522a
1047984367@qq.com <as1314520>
2019-02-26 19:40:21 +0800

    新增注释

--- src/main/java/com/pikaqiu/concurrency/example/publish/Escape.java
+++ src/main/java/com/pikaqiu/concurrency/example/publish/Escape.java
@@ -7,6 +7,11 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @NotThreadSafe
 @NotRecommend
+/**
+ * 对象未完成构造前  不能将它发布
+ * Escape 可能未被初始化就调用了内部类InnerClass
+ * 这时候thisCanBeEscape是没有初始化的
+ */
 public class Escape {
 
     private int thisCanBeEscape = 0;
--- src/main/java/com/pikaqiu/concurrency/example/publish/UnsafePublish.java
+++ src/main/java/com/pikaqiu/concurrency/example/publish/UnsafePublish.java
@@ -7,6 +7,10 @@ import java.util.Arrays;
 
 @Slf4j
 @NotThreadSafe
+/**
+ * 多线程调用时 可以拿到states数组的引用
+ * 这时候可能被其他线程修改内部数据
+ */
 public class UnsafePublish {
 
     private String[] states = {"a", "b", "c"};
