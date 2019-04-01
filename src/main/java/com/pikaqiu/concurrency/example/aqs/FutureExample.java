b34440b48132d2e4b5d7a9ea2acf1180f6389e58
1047984367@qq.com <as1314520>
2019-04-01 10:32:42 +0800

    新增注释

--- src/main/java/com/pikaqiu/concurrency/example/aqs/FutureExample.java
+++ src/main/java/com/pikaqiu/concurrency/example/aqs/FutureExample.java
@@ -27,5 +27,6 @@ public class FutureExample {
         Thread.sleep(1000);
         String result = future.get();
         log.info("result：{}", result);
+        executorService.shutdown();
     }
 }
