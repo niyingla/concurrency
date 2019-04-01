11b75884261216283608a70a91ebaa6f6f28d4c8
pikaqiu <1047984367@qq.com>
2019-04-02 00:18:33 +0800

    增加注释

--- src/main/java/com/pikaqiu/concurrency/example/cache/GuavaCacheExample1.java
+++ src/main/java/com/pikaqiu/concurrency/example/cache/GuavaCacheExample1.java
@@ -3,19 +3,26 @@ package com.pikaqiu.concurrency.example.cache;
 import com.google.common.cache.CacheBuilder;
 import com.google.common.cache.CacheLoader;
 import com.google.common.cache.LoadingCache;
+import com.pikaqiu.concurrency.annoations.ThreadSafe;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.concurrent.TimeUnit;
 
 @Slf4j
+@ThreadSafe
 public class GuavaCacheExample1 {
-
+    /**
+     * segment 细粒度锁 数据结构类似于map + 缓存过期 动态加载 等
+     * 超过最大值时 适用LRU算法来移除 可以统计命中率 未命中率 异常率
+     * @param args
+     */
     public static void main(String[] args) {
 
         LoadingCache<String, Integer> cache = CacheBuilder.newBuilder()
                 .maximumSize(10) // 最多存放10个数据
                 .expireAfterWrite(10, TimeUnit.SECONDS) // 缓存10秒
                 .recordStats() // 开启记录状态数据功能
+                //指定加载数据方法
                 .build(new CacheLoader<String, Integer>() {
                     @Override
                     public Integer load(String key) throws Exception {
@@ -47,8 +54,9 @@ public class GuavaCacheExample1 {
 
             log.info("{}", cache.get("key5")); // -1
 
+            // 打印命中数率和未命中数
             log.info("{},{}", cache.stats().hitCount(), cache.stats().missCount());
-
+            // 打印命中率和未命中率
             log.info("{},{}", cache.stats().hitRate(), cache.stats().missRate());
         } catch (Exception e) {
             log.error("cache exception", e);
