a63d852ad930237159ea29913a02238e796f84f1
1047984367@qq.com <as1314520>
2019-08-15 19:27:53 +0800

    新增注释

--- src/main/java/com/pikaqiu/concurrency/example/hystrix/HystrixController2.java
+++ src/main/java/com/pikaqiu/concurrency/example/hystrix/HystrixController2.java
@@ -18,17 +18,15 @@ public class HystrixController2 {
      * 值在线程池被创建后就固定了大小，如果需要动态修改队列长度的话可以设置此值，
      * 即使队列未满，队列内作业达到此值时同样会拒绝请求。此值默认是 5，所以有时候只设置了 
      * maxQueueSize 也不会起作用。
+     * 此属性设置队列大小拒绝阈值 - 即使未达到maxQueueSize也将发生拒绝的人为最大队列大小
      *
-     * keepAliveTimeMinutes：由上面的 maximumSize，
+     * keepAliveTimeMinutes：由下面的 maximumSize，
      * 我们知道，线程池内核心线程数目都在忙碌，再有新的请求到达时，
      * 线程池容量可以被扩充为到最大数量，等到线程池空闲后，多于核心数量的线程还会被回收，
      * 此值指定了线程被回收前的存活时间，默认为 2，即两分钟。
-     *
-     * queueSizeRejectionThreshold
-     * 此属性设置队列大小拒绝阈值 - 即使未达到maxQueueSize也将发生拒绝的人为最大队列大小
-     *
      * keepAliveTimeMinutes
      * 此属性设置保持活动时间，以分钟为单位。
+     * 
      * maximumSize 线程池最大值 不开启拒绝HystrixCommand的情况下支持的最大并发数
      * maxQueueSize 请求等待队列  coresize<maxQueueSize<maximumSize
      * 求数小于最大线程数，却大于核心线程数的时候，会一起处理所有的请求，
