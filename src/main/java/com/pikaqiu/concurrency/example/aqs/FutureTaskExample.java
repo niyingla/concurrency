41d7d8d5af3747639e71ce875d044760ad84b4b9
1047984367@qq.com <as1314520>
2019-10-21 16:14:42 +0800

    新增注释

--- src/main/java/com/pikaqiu/concurrency/example/aqs/FutureTaskExample.java
+++ src/main/java/com/pikaqiu/concurrency/example/aqs/FutureTaskExample.java
@@ -2,7 +2,6 @@ package com.pikaqiu.concurrency.example.aqs;
 
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.concurrent.Callable;
 import java.util.concurrent.FutureTask;
 
 @Slf4j
@@ -14,13 +13,10 @@ public class FutureTaskExample {
      * @throws Exception
      */
     public static void main(String[] args) throws Exception {
-        FutureTask<String> futureTask = new FutureTask<String>(new Callable<String>() {
-            @Override
-            public String call() throws Exception {
-                log.info("do something in callable");
-                Thread.sleep(5000);
-                return "Done";
-            }
+        FutureTask<String> futureTask = new FutureTask<>(() -> {
+            log.info("do something in callable");
+            Thread.sleep(5000);
+            return "Done";
         });
 
         new Thread(futureTask).start();
