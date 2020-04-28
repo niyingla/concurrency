f3c7e7729ff47d558e9b542582c2e6c47c7e55f8
1047984367@qq.com <as1314520>
2020-04-28 10:34:31 +0800

    新增注释

--- src/main/java/com/pikaqiu/concurrency/example/sync/SynchronizedExample1.java
+++ src/main/java/com/pikaqiu/concurrency/example/sync/SynchronizedExample1.java
@@ -9,6 +9,17 @@ import java.util.concurrent.Executors;
 public class SynchronizedExample1 {
 
     /**
+     * synchronized全流程
+     * 1 在代码进入同步块的时候，如果同步对象锁状态为无锁状态，前线程的栈帧中建立一个名为锁记录
+     * （Lock Record），拷贝对象头中的Mark Word复制到锁记录中,此时处于偏向锁
+     * 2 使用CAS操作尝试将对象的Mark Word更新为指向Lock Record的指针，
+     * 并将Lock record里的owner指针指向object mark word。
+     * 3 如果更新成功，那么这个线程就拥有了该对象的锁，
+     * 并且对象Mark Word的锁标志位设置为“00”，即表示此对象处于轻量级锁定状态
+     * 4 如果这个更新操作失败了，说明多个线程竞争锁，轻量级锁就要膨胀为重量级锁
+     * 在锁膨胀时， 被锁对象的 mark word 会被通过 CAS 操作尝试更新为一个数据结构的指针，
+     * 这个数据结构中进一步包含了指向操作系统互斥量(mutex) 和 条件变量（condition variable） 的指针
+     *
      * 修饰一个代码块
      * 只作用于当前对象 不同对象不影响
      * 当代码块包含了全部方法代码时 两种用法效果等同
