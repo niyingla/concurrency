d438788a5336d83843f5e7f020889642b9a93a5f
火火 <1047984367@qq.com>
2021-04-14 12:37:30 +0800

    测试提交

--- src/main/java/com/pikaqiu/concurrency/example/atomic/AtomicExample5.java
+++ src/main/java/com/pikaqiu/concurrency/example/atomic/AtomicExample5.java
@@ -18,7 +18,7 @@ public class AtomicExample5 {
      * 创建AtomicInteger字段修改类 并指定修改的字段
      * 使用方法类似于AtomicInteger
      */
-    private static AtomicIntegerFieldUpdater<AtomicExample5> updater = AtomicIntegerFieldUpdater
+    private static final AtomicIntegerFieldUpdater<AtomicExample5> UPDATER = AtomicIntegerFieldUpdater
             .newUpdater(AtomicExample5.class, "count");
 
     /**
@@ -32,11 +32,11 @@ public class AtomicExample5 {
         //指定需要更新值的对象
         AtomicExample5 example5 = new AtomicExample5();
         //和目标值进行比较 一致就修改
-        if (updater.compareAndSet(example5, 100, 120)) {
+        if (UPDATER.compareAndSet(example5, 100, 120)) {
             log.info("update success 1, {}", example5.getCount());
         }
         //和目标值进行比较 一致就修改
-        if (updater.compareAndSet(example5, 100, 120)) {
+        if (UPDATER.compareAndSet(example5, 100, 120)) {
             log.info("update success 2, {}", example5.getCount());
         } else {
             log.info("update failed, {}", example5.getCount());
